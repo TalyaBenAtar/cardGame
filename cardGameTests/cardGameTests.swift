a9091b215ee3d051ab8537ce1763ba700b6261f9
Student30 <student30@Imac8s-Mac-mini.local>
2026-06-11 15:52:39 +0300

    Initial commit~

---
+++ cardGameTests/cardGameTests.swift
@@ -0,0 +1,17 @@
+//
+//  cardGameTests.swift
+//  cardGameTests
+//
+//  Created by Student30 on 10/06/2026.
+//
+
+import Testing
+@testable import cardGame
+
+struct cardGameTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
